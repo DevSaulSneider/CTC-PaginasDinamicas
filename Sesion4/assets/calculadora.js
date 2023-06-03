bb48322d55d14ba34c70b95b93a42aea0e5fcca0
saulsneider7 <saulsneider@hotmail.com>
2023-06-02 19:11:53 -0500

    funciones calculadora

--- Sesion4/assets/calculadora.js
+++ Sesion4/assets/calculadora.js
@@ -16,13 +16,49 @@ function calcularSuma() {
 };
 
 function calcularResta() {
+    // Capturamos el valor ingresado por el usuario
+    let valor1 = document.getElementById('valor1').value;
+    let valor2 = document.getElementById('valor2').value;
+
+    //Transformamos a numero
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+
+    //Realizamos la sustraccion
+    let resulado = valor1 - valor2;
     
+    //Mostramos en consola
+    console.log(resulado);
 }
 
 function calcularProducto() {
+    // Capturamos el valor ingresado por el usuario
+    let valor1 = document.getElementById('valor1').value;
+    let valor2 = document.getElementById('valor2').value;
+
+    //Transformamos a numero
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+
+    //Realizamos la multiplicacion
+    let resulado = valor1 * valor2;
     
+    //Mostramos en consola
+    console.log(resulado);
 }
 
 function calcularDivision() {
+    // Capturamos el valor ingresado por el usuario
+    let valor1 = document.getElementById('valor1').value;
+    let valor2 = document.getElementById('valor2').value;
+
+    //Transformamos a numero
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+
+    //Realizamos la division
+    let resulado = valor1 / valor2;
     
+    //Mostramos en consola
+    console.log(resulado);
 }
