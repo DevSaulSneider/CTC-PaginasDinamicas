a8a45ff61fb710c4d8d4d6ce4f1cebe3bbc04f9e
saulsneider7 <saulsneider@hotmail.com>
2023-06-16 15:45:20 -0500

    Recursos Sesion 6

---
+++ Sesion6/assets/js/extra.js
@@ -0,0 +1,5 @@
+// Facilitar a los y las estudiantes la carpeta para_estudiantes, que contiene:
+// laberinto.xlsx - plantilla para generar laberintos
+// laberinto.txt - resultado de exportar laberinto.xlsx a .txt
+// mazeReader.js - lector de laberinto.txt
+// laberinto.js - contiene variable patronLaberinto, la cual se debe utilizar para dibujar el laberinto en canvasMaze
