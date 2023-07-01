469648890a1872c31b5be5d0539a37a13423c3f4
saulsneider7 <saulsneider@hotmail.com>
2023-06-30 19:53:18 -0500

    sesion 7 - complet

---
+++ sesion7/assets/js/parte2.js
@@ -0,0 +1,29 @@
+// Ahora dibujamos el laberinto
+function drawWalls() {
+  //Vamos a crear un cuadro de muro por cada elemento que se encuentra en el archivo laberinto.js
+
+  for (let i = 0; i < patronLaberinto.length; i++) {
+    let x = patronLaberinto [i][0] * squareSize;
+    let y = patronLaberinto [i][1] * squareSize;
+
+    canvasMaze.beginPath();
+    canvasMaze.rect(x, y, squareSize, squareSize);
+    canvasMaze.fillStyle = "blue";
+    canvasMaze.strokeStyle= "white";
+    canvasMaze.fill();
+    canvasMaze.stroke();
+  }
+}
+
+drawWalls();
+
+//Ahora vamos a spbreescribir las funciones de mover
+//Escenario
+const canvasWidth = document.getElementById("canvas-maze").width / squareSize;
+const canvasHeight = document.getElementById("canvas-maze").heidht / squareSize;
+
+function moveRight_Pacman() {
+  if(xPacman < canvasWidth -1){
+    xPacman += 1;
+  }
+}
