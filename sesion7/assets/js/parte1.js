02fcccc8594ceeea56ccba6a514120ebbd65deae
saulsneider7 <saulsneider@hotmail.com>
2023-06-30 19:32:44 -0500

    sesion 7 - parte 1

--- sesion7/assets/js/parte1.js
+++ sesion7/assets/js/parte1.js
@@ -32,17 +32,43 @@ function drawPacman() {
 }
 
 function moveRight_Pacman() {
-  
+  xPacman += 1;
 }
 
 function moveLeft_Pacman() {
-  
+  xPacman -= 1;
 }
 
 function moveUp_Pacman() {
-  
+  yPacman -= 1;
 }
 
 function moveDown_Pacman() {
-  
+  yPacman += 1;
+}
+
+//Ahora agregamos codigo que escuche cuando presiamos el teclado indicado;
+
+window.onkeydown = function (e) {
+  if (e.code == "ArrowRight") {
+    moveRight_Pacman();
+  } 
+  //Hacemos lo mismo con las demas flechas
+  else if (e.code == "ArrowLeft") {
+    moveLeft_Pacman();
+  }
+  else if(e.code == "ArrowUp"){
+    moveUp_Pacman();
+  }
+  else if(e.code == "ArrowDown"){
+    moveDown_Pacman();
+  }
+  drawGame();
+}
+
+//Cuando se ejecute la funcion drawPacman(), se dibujara un nuevo pacaman adicional
+//para corregir ese error se crearemos la funcion drawGame.
+function drawGame() {
+  canvasSprites.clearRect(0, 0, 500, 500);
+  drawPacman();
 }
